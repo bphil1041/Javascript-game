c3c8c10c2701adb556a233608d851d31642c7d83
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-09 14:49:38 -0600

    Update script.js

--- script.js
+++ script.js
@@ -15,6 +15,21 @@ window.addEventListener('load', function () {
 
     }
     class Player {
+        constructor(game) {
+            this.game = this.game;
+            this.width = 120;
+            this.height = 190;
+            this.x = 20;
+            this.y = 100;
+            this.y += this.speedY;
+        }
+        update() {
+            this.y += this.speedY;
+        }
+        draw(context) {
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
+
 
     }
     class Enemy {
@@ -30,7 +45,19 @@ window.addEventListener('load', function () {
 
     }
     class Game {
-
+        constructor(width, height) {
+            this.width = width;
+            this.height = height;
+            this.player = new Player(this);
+        }
+        update() {
+            this.player.update();
+        }
+        draw() {
+            this.player.draw(context);
+        }
     }
+
+    const game = new Game(canvas.width, canvas.height);
 
 });
