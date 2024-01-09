4385c507c547976f87d84c0791eb1689643c86d7
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-09 14:29:51 -0600

    Initial commit

---
+++ script.js
@@ -0,0 +1,36 @@
+window.addEventListener('load', function () {
+    //canvas setup
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+    canvas.width = 1500;
+    canvas.height = 500;
+
+    class InputHandler {
+
+    }
+    class Projectile {
+
+    }
+    class Particle {
+
+    }
+    class Player {
+
+    }
+    class Enemy {
+
+    }
+    class Layer {
+
+    }
+    class Background {
+
+    }
+    class UI {
+
+    }
+    class Game {
+
+    }
+
+});
