4b436ce0b4e27c2fd42ee3c5dd52c039b230e269
bphil1041 <benjamin.r.phillips1041@gmail.com>
2024-01-09 19:42:37 -0600

    allow player to move horizontally and add projectiles

--- script.js
+++ script.js
@@ -9,28 +9,45 @@ window.addEventListener('load', function () {
         constructor(game) {
             this.game = game;
             window.addEventListener('keydown', e => {
-                if (((e.key === 'ArrowUp') ||
-                    (e.key === 'ArrowDown')
-                ) && this.game.keys.indexOf(e.key) === -1) {
+                if (['ArrowUp', 'ArrowDown', 'ArrowLeft', 'ArrowRight'].includes(e.key) &&
+                    this.game.keys.indexOf(e.key) === -1) {
                     this.game.keys.push(e.key);
+                } else if (e.key === ' ') {
+                    this.game.player.shootTop();
                 }
-                console.log(this.game.keys);
             });
             window.addEventListener('keyup', e => {
                 if (this.game.keys.indexOf(e.key) > -1) {
                     this.game.keys.splice(this.game.keys.indexOf(e.key), 1);
                 }
-                console.log(this.game.keys);
             });
         }
-
     }
+
     class Projectile {
+        constructor(game, x, y) {
+            this.game = game;
+            this.x = x;
+            this.y = y;
+            this.width = 10;
+            this.height = 3;
+            this.speed = 15;
+            this.markedForDeletion = false;
+        }
 
-    }
-    class Particle {
+        update() {
+            this.x += this.speed;
+            if (this.x > this.game.width * 0.9) this.markedForDeletion = true;
+        }
 
+        draw(context) {
+            context.fillStyle = 'yellow';
+            context.fillRect(this.x, this.y, this.width, this.height);
+        }
     }
+
+    class Particle { }
+
     class Player {
         constructor(game) {
             this.game = game;
@@ -38,33 +55,60 @@ window.addEventListener('load', function () {
             this.height = 190;
             this.x = 20;
             this.y = 100;
+            this.speedX = 0;
             this.speedY = 0;
-            this.maxSpeed = 2;
+            this.maxSpeed = 4;
+            this.projectiles = [];
         }
+
         update() {
-            if (this.game.keys.includes('ArrowUp')) this.speedY = -1;
-            else if (this.game.keys.includes('ArrowDown')) this.speedY = 1;
+            if (this.game.keys.includes('ArrowUp')) this.speedY = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowDown')) this.speedY = this.maxSpeed;
             else this.speedY = 0;
+
+            if (this.game.keys.includes('ArrowLeft')) this.speedX = -this.maxSpeed;
+            else if (this.game.keys.includes('ArrowRight')) this.speedX = this.maxSpeed;
+            else this.speedX = 0;
+
+            // Update player position
+            this.x += this.speedX;
             this.y += this.speedY;
+
+            // Ensure player stays within the canvas boundaries
+            this.x = Math.max(0, Math.min(canvas.width - this.width, this.x));
+            this.y = Math.max(0, Math.min(canvas.height - this.height, this.y));
+
+            // Handle projectiles
+            this.projectiles.forEach(projectile => {
+                projectile.update();
+            });
+            this.projectiles = this.projectiles.filter(projectile => !projectile.markedForDeletion);
         }
+
         draw(context) {
+            context.fillStyle = 'black';
             context.fillRect(this.x, this.y, this.width, this.height);
+
+            // Render projectiles
+            this.projectiles.forEach(projectile => {
+                projectile.draw(context);
+            });
         }
 
-
+        shootTop() {
+            this.projectiles.push(new Projectile(this.game, this.x, this.y));
+            console.log(this.projectiles);
+        }
     }
-    class Enemy {
 
-    }
-    class Layer {
+    class Enemy { }
 
-    }
-    class Background {
+    class Layer { }
 
-    }
-    class UI {
+    class Background { }
 
-    }
+    class UI { }
+
     class Game {
         constructor(width, height) {
             this.width = width;
@@ -73,9 +117,11 @@ window.addEventListener('load', function () {
             this.input = new InputHandler(this);
             this.keys = [];
         }
+
         update() {
             this.player.update();
         }
+
         draw(context) {
             this.player.draw(context);
         }
@@ -89,6 +135,6 @@ window.addEventListener('load', function () {
         game.draw(ctx);
         requestAnimationFrame(animate);
     }
-    animate();
 
+    animate();
 });
